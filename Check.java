b474a7a7983fbfc64de663a8ea4f2340ddc29001
Valentin <mv77270@gmail.com>
2017-04-10 18:31:42 +0200

    Add tests for comparators

--- Check.java
+++ Check.java
@@ -7,48 +7,62 @@ public class Check {
 	public static final String ANSI_GREEN = "\u001B[32m";
 
 	public static void main(String[] args) throws AssertionError {
-			System.out.println("1 + 4 =? 5");
-			assert Evaluateur.visit(new Add(new Num(1), new Num(4))) == 5.0;
+			Exp foo = new Eq(new Add(new Num(1), new Num(4)), new Num(5)); // 1 + 4 = 5
+			System.out.println(PrettyPrinter.visit(foo) + " ?");
+			assert Evaluateur.visit(foo) == 1.0;
 			System.out.println(ANSI_GREEN + "\t\t\t\t\t\t\t\tOk" + ANSI_RESET);
 
 
-			System.out.println("1 - 4 =? -3");
-			assert Evaluateur.visit(new Sub(new Num(1), new Num(4))) == -3.0;
+			foo = new Eq(new Sub(new Num(1), new Num(4)), new Num(-3)); // 1 - 4 = -3
+			System.out.println(PrettyPrinter.visit(foo) + " ?");
+			assert Evaluateur.visit(foo) == 1.0;
 			System.out.println(ANSI_GREEN + "\t\t\t\t\t\t\t\tOk" + ANSI_RESET);
 
 
-			System.out.println("1 * 4 =? 4");
-			assert Evaluateur.visit(new Mul(new Num(1), new Num(4))) == 4.0;
+			foo = new Eq(new Mul(new Num(1), new Num(4)), new Num(4)); // 1 * 4 = 4
+			System.out.println(PrettyPrinter.visit(foo) + " ?");
+			assert Evaluateur.visit(foo) == 1.0;
+			System.out.println(ANSI_GREEN + "\t\t\t\t\t\t\t\tOk" + ANSI_RESET);
+
+			foo = new Eq(new Div(new Num(1), new Num(4)), new Num(0.25)); // 1 / 4 = 0.25
+			System.out.println(PrettyPrinter.visit(foo) + " ?");
+			assert Evaluateur.visit(foo) == 1.0;
 			System.out.println(ANSI_GREEN + "\t\t\t\t\t\t\t\tOk" + ANSI_RESET);
 
 
-			System.out.println("1 / 4 =? 4");
-			assert Evaluateur.visit(new Div(new Num(1), new Num(4))) == 1.0/4.0;
+			foo = new Eq(new Add(new Num(1),new Mul( new Num(4), new Num(2))), new Num(9)); // 1 + 4 * 2 = 9
+			System.out.println(PrettyPrinter.visit(foo) + " ?");
+			assert Evaluateur.visit(foo) == 1.0;
 			System.out.println(ANSI_GREEN + "\t\t\t\t\t\t\t\tOk" + ANSI_RESET);
 
 
-			System.out.println("1 + 4 * 2 =? 9");
-			assert Evaluateur.visit(new Add(new Num(1),new Mul( new Num(4), new Num(2)))) == 9.0;
+			foo = new Eq(new Mul(new Add(new Num(1), new Num(4)), new Num(2)), new Num(10)); // (1 + 4) * 2 = 10
+			System.out.println(PrettyPrinter.visit(foo) + " ?");
+			assert Evaluateur.visit(foo) == 1.0;
 			System.out.println(ANSI_GREEN + "\t\t\t\t\t\t\t\tOk" + ANSI_RESET);
 
 
-			System.out.println("(1 + 4) * 2 =? 10");
-			assert Evaluateur.visit(new Mul(new Add(new Num(1), new Num(4)), new Num(2))) == 10;
+			foo = new Eq(new Add(new Add(new Num(1), new Num(2)), new Num(3)), new Num(6)); // 1 + 2 + 3 = 6
+			System.out.println(PrettyPrinter.visit(foo) + " ?");
+			assert Evaluateur.visit(foo) == 1.0;
 			System.out.println(ANSI_GREEN + "\t\t\t\t\t\t\t\tOk" + ANSI_RESET);
 
 
-			System.out.println("1 + 2 + 3 =? 6");
-			assert Evaluateur.visit(new Add(new Add(new Num(1), new Num(2)), new Num(3))) == 6.0;
+			foo = new Eq(new Add(new Minus(new Num(8)), new Minus(new Num(7))), new Num(-15)); // -8 + -7 = -15
+			System.out.println(PrettyPrinter.visit(foo) + " ?");
+			assert Evaluateur.visit(foo) == 1.0;
 			System.out.println(ANSI_GREEN + "\t\t\t\t\t\t\t\tOk" + ANSI_RESET);
 
 
-			System.out.println("-8 + -7 =? -15");
-			assert Evaluateur.visit(new Add(new Minus(new Num(8)), new Minus(new Num(7)))) == -15.0;
+			foo = new Eq(new Minus(new Add(new Num(42), new Num(51))), new Num(-93)); // -(42 + 51) = -93
+			System.out.println(PrettyPrinter.visit(foo) + " ?");
+			assert Evaluateur.visit(foo) == 1.0;
 			System.out.println(ANSI_GREEN + "\t\t\t\t\t\t\t\tOk" + ANSI_RESET);
 
 
-			System.out.println("-(42 + 51) =? -93");
-			assert Evaluateur.visit(new Minus(new Add(new Num(42), new Num(51)))) == -93.0;
+			foo = new Lt(new Add(new Minus(new Mul(new Num(3), new Num(7))), new Num(28)), new Num(14)); // -(3 * 7) + 28 < 14
+			System.out.println(PrettyPrinter.visit(foo) + " ?");
+			assert Evaluateur.visit(foo) == 1.0;
 			System.out.println(ANSI_GREEN + "\t\t\t\t\t\t\t\tOk" + ANSI_RESET);
 	}
 }
