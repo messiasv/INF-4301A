78158efe84df92e4b8f2e800de297304ef561596
Valentin <mv77270@gmail.com>
2017-04-10 18:05:06 +0200

    Add PrettyPrint for comp operators

--- PrettyPrinter.java
+++ PrettyPrinter.java
@@ -17,6 +17,18 @@ public class PrettyPrinter{
 				return visit(((Unary)e).getFactor());
 			case "Minus":
 				return "-" + visit(((Unary)e).getFactor());
+			case "Eq":
+				return visit(((Bin)e).getLeft()) +" = "+ visit(((Bin)e).getRight());
+			case "Ne":
+				return visit(((Bin)e).getLeft()) +" <> "+ visit(((Bin)e).getRight());
+			case "Le":
+				return visit(((Bin)e).getLeft()) +" <= "+ visit(((Bin)e).getRight());
+			case "Lt":
+				return visit(((Bin)e).getLeft()) +" < " + visit(((Bin)e).getRight());
+			case "Ge":
+				return visit(((Bin)e).getLeft()) +" > " + visit(((Bin)e).getRight());
+			case "Gt":
+				return visit(((Bin)e).getLeft()) +" > " + visit(((Bin)e).getRight());
 		}
 		return e.getClass().getSimpleName();
 	}
